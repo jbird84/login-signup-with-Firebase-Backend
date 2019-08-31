2673e8fc0a534562ae371e1ac6bccbf9d27c2dba
Josh Kinney <joshkinney@kinneykare.com>
2019-08-30 22:36:15 -0500

    Initial Commit

---
+++ IBuiltTinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBuiltTinder
+//
+//  Created by Kinney Kare on 8/30/19.
+//  Copyright © 2019 Kinney Kare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
